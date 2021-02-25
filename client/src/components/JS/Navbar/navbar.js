fc58cbe6854857242ffb060e7878764cef48b412
Akhil Suresh <akhilf4@gmail.com>
2021-02-25 12:11:46 +0530

    converted navbar to class

--- client/src/components/JS/Navbar/navbar.js
+++ client/src/components/JS/Navbar/navbar.js
@@ -5,57 +5,13 @@ import { SidebarData } from './SidebarData';
 import { IconContext } from 'react-icons';
 import { Grid } from '@material-ui/core';
 import { Avatar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-
-/**
- * Creates the header (navbar), sidebar with avatar component and its menu items.
- */
-
-function Navbar() {
-  const classes = useStyles();
-  const [link,setLink] = useState('Dashboard')
-
-  
-  return (
-    <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navbar'>
-          <Grid container justify="flex-end" style={{alignItems: 'center'}}>
-            <button className = "logout-btn" name="logout" onClick={() => { alert('Logged Out!') }}><span>Logout</span></button>
-          </Grid>
-        </div>
-        <div className='nav-menu'>
-          <ul className='nav-menu-items active'>
-          <div className='avatar-item'>
-            <div className={classes.root} >
-              <Avatar alt="Jordan" src="./components/images/jordan-avatar.jpg" className={classes.large}/>
-            </div>
-            <h4 className='avatar'>Jordan</h4>
-          </div>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName} id={item.title == link ? "active": ""} onClick={()=>setLink(item.title)}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </IconContext.Provider>
-    </>
-  );
-}
-
+import { withStyles } from '@material-ui/core/styles';
 
 /**
  * custom class for circular avatar and its size.
  */
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = theme => ({
   root: {
     display: 'flex',
     '& > *': {
@@ -66,7 +22,56 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(9),
     height: theme.spacing(9),
   },
-}));
+});
 
 
-export default Navbar;
+/**
+ * Creates the header (navbar), sidebar with avatar component and its menu items.
+ */
+
+class Navbar extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={link:'Dashboard'};
+  
+  //const [link,setLink] = useState('Dashboard')
+  }
+  
+  render(){
+    const {classes} = this.props;
+    return (
+      <>
+        <IconContext.Provider value={{ color: '#fff' }}>
+          <div className='navbar'>
+            <Grid container justify="flex-end" style={{alignItems: 'center'}}>
+              <button className = "logout-btn" name="logout" onClick={() => { alert('Logged Out!') }}><span>Logout</span></button>
+            </Grid>
+          </div>
+          <div className='nav-menu'>
+            <ul className='nav-menu-items active'>
+            <div className='avatar-item'>
+              <div className={classes.root} >
+                <Avatar alt="Jordan" src="./components/images/jordan-avatar.jpg" className={classes.large}/>
+              </div>
+              <h4 className='avatar'>Jordan</h4>
+            </div>
+              {SidebarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName} id={item.title == this.state.link ? "active": ""} onClick={()=>this.setState({link:item.title})}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </IconContext.Provider>
+      </>
+    );
+  }
+}
+
+
+export default withStyles(useStyles)(Navbar);
