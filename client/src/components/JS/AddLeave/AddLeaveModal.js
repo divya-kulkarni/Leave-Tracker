e3e0862cbd78ce3543c6d2fb3fa356f0556e9cfc
Divya Kulkarni <44868354+divya-kulkarni@users.noreply.github.com>
2021-02-25 16:03:34 +0530

    fixed modal CSS

--- client/src/components/JS/AddLeave/AddLeaveModal.js
+++ client/src/components/JS/AddLeave/AddLeaveModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../CSS/addLeaveBtn.css';
 import { Button, Form, Modal } from 'react-bootstrap';
 import DatePicker from 'react-date-picker';
-import {isWithinInterval,addDays} from 'date-fns';
+import { isWithinInterval, addDays } from 'date-fns';
 
 class AddLeaveModal extends React.Component {
   constructor(props) {
@@ -13,19 +13,19 @@ class AddLeaveModal extends React.Component {
       employee_id: null,
       endDate: ''
     }
-    
+
     this.handleModal = this.handleModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.tileToDisable = this.tileToDisable.bind(this);
-    
+
   }
 
   handleChange(event) {
     const value = event.target.value;
-    const name= event.target.name;
+    const name = event.target.name;
     this.setState({
-      [name] : value
+      [name]: value
     });
   }
 
@@ -40,27 +40,27 @@ class AddLeaveModal extends React.Component {
   async handleSubmit(event) {
     event.preventDefault();
     console.log(this.state);
-    console.log('employee id: ' + this.state.employee_id + '  start date:'+this.state.startDate + ' end date:'+this.state.endDate);
-    const result = await fetch("http://localhost:5000/addLeave",{
-        method:"POST",
-        headers:{ "Content-Type": "application/json" },
-        body:JSON.stringify(this.state)
+    console.log('employee id: ' + this.state.employee_id + '  start date:' + this.state.startDate + ' end date:' + this.state.endDate);
+    const result = await fetch("http://localhost:5000/addLeave", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.state)
     });
-    const data=await result.json();
+    const data = await result.json();
     console.log(data);
-    this.setState({show:false});
+    this.setState({ show: false });
     this.props.refresh();
     //console.log(this.props);
-}
+  }
 
-tileToDisable(date){
-  var flag =false;
-  this.props.leaves.forEach(ele => {
-    if(isWithinInterval(date.date,{start:new Date(ele.start_date),end:addDays( new Date(ele.start_date),ele.count-1)}))
-        flag=true;
-  });
-  return flag;
-}
+  tileToDisable(date) {
+    var flag = false;
+    this.props.leaves.forEach(ele => {
+      if (isWithinInterval(date.date, { start: new Date(ele.start_date), end: addDays(new Date(ele.start_date), ele.count - 1) }))
+        flag = true;
+    });
+    return flag;
+  }
 
   render() {
     return (
@@ -69,20 +69,33 @@ tileToDisable(date){
         <Modal className='LeaveModal' show={this.state.show} onHide={() => { this.handleModal() }}>
           <Modal.Header>
             Add Leave To Track Risk
-            <Button className="btn btn-default add-leave" onClick={() => { this.handleModal() }}>&times;</Button>
+            <Button className="btn btn-default add-leave" 
+            onClick={() => { this.handleModal() }}>&times;</Button>
           </Modal.Header>
           <Modal.Body>
-            <Form onSubmit={this.handleSubmit}>
+            <Form className='leave-form' onSubmit={this.handleSubmit}>
               <Form.Group controlId="addLeaveForm">
-              <Form.Label>Employee ID</Form.Label>
-                <Form.Control name="employee_id" type="text" onChange={this.handleChange}/>
+                {/* <Form.Label>Employee ID</Form.Label> */}
+                <Form.Control name="employee_id" type="text" 
+                onChange={this.handleChange} placeholder='Employee ID' />
                 <br />
-                <Form.Label>Start Date</Form.Label>
-                <DatePicker name='startdate' className="start-date" value={this.state.startDate} onChange={value => this.setState({ startDate: value })} minDate={new Date()} tileDisabled={this.tileToDisable}/>
-                <br />
-                <Form.Label>End Date</Form.Label>
-                <DatePicker name='enddate' className="end-date" value={this.state.endDate} onChange={value => this.setState({ endDate: value })} minDate={new Date(this.state.startDate)} tileDisabled={this.tileToDisable}/>
-
+                <div className='datePicker'>
+                  <Form.Label>Start Date</Form.Label>
+                  <br />
+                  <DatePicker name='startdate' className="start-date" 
+                  value={this.state.startDate} onChange={value => this.setState({ startDate: value })} 
+                  minDate={new Date()} tileDisabled={this.tileToDisable} 
+                    format='d/MM/y'
+                  />
+                  <br />
+                  <Form.Label>End Date</Form.Label>
+                  <br />
+                  <DatePicker name='enddate' className="end-date" 
+                  value={this.state.endDate} onChange={value => this.setState({ endDate: value })} 
+                  minDate={new Date(this.state.startDate)} tileDisabled={this.tileToDisable} 
+                  format='d/MM/y'
+                  />
+                </div>
               </Form.Group>
               <Button className="btn btn-default submitBtn" type="submit" id="addLeaveBtn">
                 Add
