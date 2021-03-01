becfc64108fe9c0a25a4b22891c8abe7a3f01379
17Sruthi <78374070+17Sruthi@users.noreply.github.com>
2021-03-01 20:16:17 +0530

    login small validation done

--- server/index.js
+++ server/index.js
@@ -15,37 +15,45 @@ app.use(express.json());
 app.post('/login',(req,res)=>{
     var empid = req.body.employee_id;
     var pass = req.body.pass;
-    try{
-        conn.query('SELECT * FROM employee WHERE employee_id = ? LIMIT 1',[empid],(err, result)=>{
-            if(err)   
-                res.json({
-                    success : false,
-                    message : err
-                });
-
-            if(result.length == 0)
-                res.json({
-                    success : false,
-                    message : 'No such user,Check your id or contact Admin'
-                });
-        
-            if(result && result.length == 1){              
-                if(result[0].password == pass)
-                    res.json({
-                        success : true,
-                        message : 'Login success',
-                        name : result[0].employee_name
-                    });
-                else
+    if(isNaN(empid))
+        res.json({
+            success : false,
+            message : 'No such user,Check your id or contact Admin'
+        })
+    else {    
+        try{
+            conn.query('SELECT * FROM employee WHERE employee_id = ? LIMIT 1',[empid],(err, result)=>{
+                console.log(result);
+                if(err)   
                     res.json({
                         success : false,
-                        message : 'Incorrect password, try again'
+                        message : err
                     });
-            }
-        });
-    }
-    catch(err){
-        console.log(err)
+
+                if(result.length == 0)
+                    res.json({
+                        success : false,
+                        message : 'No such user,Check your id or contact Admin'
+                    });
+            
+                if(result && result.length == 1){              
+                    if(result[0].password == pass)
+                        res.json({
+                            success : true,
+                            message : 'Login success',
+                            name : result[0].employee_name
+                        });
+                    else
+                        res.json({
+                            success : false,
+                            message : 'Incorrect password, try again'
+                        });
+                }
+            });
+        }
+        catch(err){
+            console.log(err)
+        }
     }
 });
 
