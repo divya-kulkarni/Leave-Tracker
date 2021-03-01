d2f54e1bc182a3daae5cee4d4be140a219a14d58
Divya Kulkarni <44868354+divya-kulkarni@users.noreply.github.com>
2021-03-01 17:59:22 +0530

    added 404 page CSS

--- client/src/components/JS/NotFoundPage.js
+++ client/src/components/JS/NotFoundPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import image from '../CSS/error-img.png';
+
 // import PageNotFound from '../assets/images/PageNotFound';
-class NotFoundPage extends React.Component{
-    render(){
-        return <div>
-        <center>
-            <h2>404</h2>
-            <h3>Page does not exist!</h3>
-            </center>
-          </div>;
+class NotFoundPage extends React.Component {
+    render() {
+        return (
+            <div style={{ backgroundColor: '#060b26', height: '100vh', width: '100vw' }}>
+                <div style={{backgroundColor: '#060b26', paddingTop: '10vh'}}>
+                    <center style={{ color: 'white' }}>
+                        <img src={image} alt="error_image" height="300px" width= "300px"></img>
+                        <br /><br />
+                        <h4><b>The page you were looking for does not exist.</b></h4>
+                        <h6>You may have mistyped the address or the page may have moved.</h6>
+                    </center>
+                </div>
+            </div>
+        );
     }
 }
 export default NotFoundPage;
